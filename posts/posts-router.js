94f15f9fe329d77731e36ef25e07c86ff60baa1a
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-10-01 20:25:57 -0500

    post and get routes complete

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -21,13 +21,15 @@ router.post('/', (req, res) => {//Post 1 to /api/posts
 
 })
 
-router.post('/:id/comment', (req, res) => {//Post 2 to /api/posts/:id
-    const commentData = req.body;
-    const id = params.match.id;
+//Posts comment to post of specified ID
 
-    if(!id) {
-        res.status(404).json({ message: 'The post with the specified ID does not exist' })
-    } else {
+router.post('/:id/comments', (req, res) => {//Post 2 to /api/posts/:id
+    const commentData = req.body;
+    const id = req.params.id;
+
+    // if(!commentData.post_ID) {
+    //     res.status(404).json({ message: 'The post with the specified ID does not exist' })
+    // } else {
         Posts.insertComment(commentData)
         .then(post => {
             res.status(201).json(post);
@@ -35,13 +37,37 @@ router.post('/:id/comment', (req, res) => {//Post 2 to /api/posts/:id
         .catch(err => {
             res.status(500).json({ message: 'Error adding post' })
         })
-    }
+    })
 
-})
+// })
+
+//Get all posts
 
 router.get('/', (req, res) => {
     Posts.find().then(post => {
         res.json(post)
+        console.log(post)
+    })
+})
+
+//Get posts by ID
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+
+    Posts.findById(id).then(post => {
+        res.status(200).json(post)
+        console.log(post)
+    })
+})
+
+//Get all comments of a post found by ID
+
+router.get('/:id/comments', (req, res) => {
+    const PostId = req.params.id;
+    Posts.findPostComments(PostId).then(comments => {
+        res.status(200).json(comments)
+        console.log(comments)
     })
 })
 
